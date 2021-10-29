3cfb6d03c51673bc31a91f93c74f0c27082d7ed2
radibataineh17 <55924647+radibataineh17@users.noreply.github.com>
2021-10-29 20:53:04 +0200

    Add files via upload

---
+++ DevOpsInClass/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    private static double add(double x, double y)
+    {
+        return x+y;
+    }
+    public static void main(String[] args) {
+
+        double x=9.9;
+        double y=3332.4444;
+       double z=add(x,y);
+       System.out.println(z);
+    }
+}
